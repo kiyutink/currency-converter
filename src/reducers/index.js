9856bffe941ba7bbd63d4df97e4803c7e5f523ec
kiyutink <kiyutin@gmail.com>
2019-02-28 16:34:27 +0300

    Add tests to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import balances from "./balances";
-import rates from "./rates";
+import balances from "./balances/balances";
+import rates from "./rates/rates";
 
 export default combineReducers({
   balances,
